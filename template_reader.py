9d75e5a9af5384829e207a071cda4a0c9456b7b7
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-01-26 14:46:55 -0500

    Template document fields printed in the console

--- template_reader.py
+++ template_reader.py
@@ -1,8 +1,18 @@
 from PyPDF2 import PdfFileReader
 
+def print_dictionary(d):
+	for key in d:
+		print(str(key) + ": " + str(d[key]))
+
 template = PdfFileReader(open("rapport_depenses.pdf", "rb"))
 
 template_info = template.getDocumentInfo()
 print("Template document info")
-for key in template_info:
-    print(key, ": ", template_info[key])
+print_dictionary(template_info)
+
+template_fields = template.getFields()
+print("\nTemplate document fields")
+if template_fields is None:
+	print("No fields")
+else:
+	print_dictionary(template_fields)
