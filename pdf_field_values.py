6b7f1033e294dc5bb96a139e502d812c7360f448
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-01-26 16:52:00 -0500

    First argument: input file

--- pdf_field_values.py
+++ pdf_field_values.py
@@ -1,7 +1,24 @@
-from PyPDF2 import PdfFileReader
 from pathlib import Path
+from PyPDF2 import PdfFileReader
+from sys import argv, exit
 
-reader = PdfFileReader(open("rapport_depenses.pdf", "rb"))
+INPUT_EXTENSION = ".pdf"
+
+try:
+	input_path = Path(argv[1])
+except IndexError:
+	print("ERROR! Need the input file as the first argument.")
+	exit()
+
+if not input_path.exists():
+	print("ERROR! " + str(input_path) + " does not exist.")
+	exit()
+
+if not input_path.suffix == INPUT_EXTENSION:
+	print("ERROR! The input file must have the extension " + INPUT_EXTENSION + ".")
+	exit()
+
+reader = PdfFileReader(input_path.open(mode="rb"))
 field_values = reader.getFormTextFields()
 
 field_str = str()
