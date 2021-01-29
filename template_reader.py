87bfc7df7ef6edc5e23332a2ae1b261a1a876f87
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-01-29 00:18:29 -0500

    Fields updated, but value invisible when field not activated
    The solution failed because importing BooleanObject raises an ImportError.

--- template_reader.py
+++ template_reader.py
@@ -1,25 +1,41 @@
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfFileReader, PdfFileWriter
+#from PyPDF2 import BooleanObject, NameObject, PdfFileReader, PdfFileWriter
+
+#def make_field_content_visible(pdf_reader):
+	#https://github.com/mstamy2/PyPDF2/issues/355#issuecomment-353217311
+#	if "/AcroForm" in pdf_reader.trailer["/Root"]:
+#		pdf_reader.trailer["/Root"]["/AcroForm"].update(
+#			{NameObject("/NeedAppearances"): BooleanObject(True)})
+
 
 def print_dictionary(d):
 	for key, value in d.items():
 		print(str(key) + ": " + str(value))
 
-template = PdfFileReader(open("rapport_depenses.pdf", "rb"))
+
+input_stream = open("rapport_depenses.pdf", "rb")
+template = PdfFileReader(input_stream)
+writer = PdfFileWriter()
+#make_field_content_visible(template)
 
 template_info = template.getDocumentInfo()
 print("Template document info")
 print_dictionary(template_info)
 
-template_fields = template.getFields()
-print("\nTemplate document fields")
-if template_fields is None:
-	print("No fields")
-else:
-	print_dictionary(template_fields)
+#writer.cloneDocumentFromReader(template)
+#make_field_content_visible(writer)
 
-template_field_values = template.getFormTextFields()
-print("\nTemplate document field values")
-if template_field_values is None:
-	print("No fields")
-else:
-	print_dictionary(template_field_values)
+#page = writer.getPage(0)
+page = template.getPage(0)
+writer.addPage(page)
+field_update = {"Nom": "Dupré",
+				"Prenom": "Jeanne",
+				"Group2": "\Choix2",
+				"CodePermanent": "DUPJ01060901"}
+writer.updatePageFormFieldValues(page, field_update)
+
+output_stream = open("rapport_depenses_essai.pdf", "wb")
+writer.write(output_stream)
+
+input_stream.close()
+output_stream.close()
