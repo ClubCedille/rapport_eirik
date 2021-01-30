e323e4a4d697491ce63e529c6257e332a57e3d87
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-01-29 20:53:51 -0500

    Solution runs, but values are still invisible.

--- template_reader.py
+++ template_reader.py
@@ -1,11 +1,19 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
-#from PyPDF2 import BooleanObject, NameObject, PdfFileReader, PdfFileWriter
+#https://github.com/mstamy2/PyPDF2/issues/355#issuecomment-360317085
+from PyPDF2.generic import BooleanObject, NameObject
 
-#def make_field_content_visible(pdf_reader):
+def make_reader_field_content_visible(pdf_reader):
 	#https://github.com/mstamy2/PyPDF2/issues/355#issuecomment-353217311
-#	if "/AcroForm" in pdf_reader.trailer["/Root"]:
-#		pdf_reader.trailer["/Root"]["/AcroForm"].update(
-#			{NameObject("/NeedAppearances"): BooleanObject(True)})
+	if "/AcroForm" in pdf_reader.trailer["/Root"]:
+		pdf_reader.trailer["/Root"]["/AcroForm"].update(
+			{NameObject("/NeedAppearances"): BooleanObject(True)})
+
+
+def make_writer_field_content_visible(pdf_writer):
+	#https://github.com/mstamy2/PyPDF2/issues/355#issuecomment-353217311
+	if "/AcroForm" in pdf_writer._root_object:
+		pdf_writer._root_object["/AcroForm"].update(
+			{NameObject("/NeedAppearances"): BooleanObject(True)})
 
 
 def print_dictionary(d):
@@ -15,16 +23,14 @@ def print_dictionary(d):
 
 input_stream = open("rapport_depenses.pdf", "rb")
 template = PdfFileReader(input_stream)
-writer = PdfFileWriter()
-#make_field_content_visible(template)
+make_reader_field_content_visible(template)
 
 template_info = template.getDocumentInfo()
 print("Template document info")
 print_dictionary(template_info)
 
+writer = PdfFileWriter()
 #writer.cloneDocumentFromReader(template)
-#make_field_content_visible(writer)
-
 #page = writer.getPage(0)
 page = template.getPage(0)
 writer.addPage(page)
@@ -33,6 +39,7 @@ field_update = {"Nom": "Dupré",
 				"Group2": "\Choix2",
 				"CodePermanent": "DUPJ01060901"}
 writer.updatePageFormFieldValues(page, field_update)
+make_writer_field_content_visible(writer)
 
 output_stream = open("rapport_depenses_essai.pdf", "wb")
 writer.write(output_stream)
