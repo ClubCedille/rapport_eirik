97f0e97e40f1046f92157b25d1bd2a087e8123cd
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-02-23 00:32:11 -0500

    Test with fake values. None values are filtered.

--- field_value_parser.py
+++ field_value_parser.py
@@ -2,6 +2,16 @@ from pathlib import Path
 from yaml import FullLoader, load
 
 
+def filter_nones_from_dict(a_dict):
+	no_nones = dict()
+
+	for key, value in a_dict.items():
+		if value is not None:
+			no_nones[key] = value
+
+	return no_nones
+
+
 def parse_field_values(field_config_path):
 	if type(field_config_path) is not Path:
 		field_config_path = Path(field_config_path)
@@ -10,10 +20,12 @@ def parse_field_values(field_config_path):
 		return load(config_stream, FullLoader)
 
 
-def print_dictionary(d):
-	for key, value in d.items():
+def print_dictionary(a_dict):
+	for key, value in a_dict.items():
 		print(str(key) + ": " + str(value))
 
 
 if __name__ == "__main__":
-	print_dictionary(parse_field_values("field_filling.yml"))
+	field_values = parse_field_values("field_filling.yml")
+	field_values = filter_nones_from_dict(field_values)
+	print_dictionary(field_values)
