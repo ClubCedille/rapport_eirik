2ba67d0094f63ad1f2eeb18596dc516099539281
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-02-27 16:07:53 -0500

    Page objects explored recursively

--- write_page_dicts.py
+++ write_page_dicts.py
@@ -1,7 +1,54 @@
-from dict_util import write_dict_in_stream
 from pathlib import Path
 from PyPDF2 import PdfFileReader
+from PyPDF2.generic import PdfObject
 from sys import argv
+
+
+def _make_tabs(n):
+	return "\t" * n
+
+
+def _obj_and_type_to_str(obj):
+	return str(obj) + " " + str(type(obj))
+
+
+def _write_page_objs_rec(w_stream, obj_to_write, indent=0):
+	tabs = _make_tabs(indent)
+	indent += 1
+
+	if isinstance(obj_to_write, PdfObject):
+		obj_to_write = obj_to_write.getObject()
+
+	if isinstance(obj_to_write, (list, tuple)):
+		length = len(obj_to_write)
+
+		for i in range(length):
+			item = obj_to_write[i]
+
+			if isinstance(item, (dict, list, set, tuple)):
+				_write_page_objs_rec(w_stream, item, indent)
+			else:
+				w_stream.write(tabs + "[" + str(i) + "]: " + _obj_and_type_to_str(item) + "\n")
+
+	elif isinstance(obj_to_write, dict):
+		for key, value in obj_to_write.items():
+			line = tabs + str(key) + ":"
+
+			if isinstance(value, dict):
+				_write_page_objs_rec(w_stream, value, indent)
+			else:
+				line += _obj_and_type_to_str(value)
+
+			w_stream.write(line + "\n")
+
+	elif isinstance(obj_to_write, set):
+		for item in obj_to_write:
+			if isinstance(item, (dict, list, set, tuple)):
+				_write_page_objs_rec(w_stream, item, indent)
+			else:
+				w_stream.write(tabs + _obj_and_type_to_str(item) + "\n")
+
+	w_stream.write(tabs + str(obj_to_write) + "\n")
 
 
 try:
@@ -22,6 +69,6 @@ with output_path.open("w") as output_stream:
 	output_stream.write("Content of " + str(input_path) + "\n")
 
 	for i in range(len(pages)):
+		page = pages[i]
 		output_stream.write("\nPage " + str(i) + "\n")
-		write_dict_in_stream(pages[i], output_stream, after_line="\n")
-		#output_stream.write("\n")
+		_write_page_objs_rec(output_stream, page)
