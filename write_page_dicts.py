abd0d03fe7fbb0096faaef1b82f83ebddf37e3a6
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-02-27 12:47:30 -0500

    PDF file pages' keys and values written in a .txt file

---
+++ write_page_dicts.py
@@ -0,0 +1,24 @@
+from dict_util import write_dict_in_stream
+from pathlib import Path
+from PyPDF2 import PdfFileReader
+from sys import argv
+
+
+try:
+	input_path = Path(argv[1])
+except IndexError:
+	print("The path to a PDF file must be provided as an argument.")
+
+try:
+	output_path = Path(argv[2])
+except IndexError:
+	output_path = input_path.parents[0]/(input_path.stem + ".txt")
+
+reader = PdfFileReader(input_path.open("rb"))
+pages = reader.pages
+
+with output_path.open("w") as output_stream:
+	for i in range(len(pages)):
+		output_stream.write("Page " + str(i) + "\n")
+		write_dict_in_stream(pages[i], output_stream, after_line="\n")
+		output_stream.write("\n")
