996304a1880ad44a3156ccde5d900e54b41d50e6
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-09 16:42:06 -0500

    Docstrings for field_value_parser.py

--- field_value_parser.py
+++ field_value_parser.py
@@ -1,9 +1,32 @@
+"""
+Given a YAML file, this module allows to extract the name-value pairs that
+define the content of a PDF file's fields. If this module is executed with the
+path to a YAML file as its argument, it will print those pairs in the console.
+In the YAML file, the fields' name and value must be stated as follows.
+field1: value1
+field2: value2
+...
+"""
+
+
 from pathlib import Path
 from sys import argv
 from yaml import FullLoader, load
 
 
 def filter_values_from_dict(a_dict, unwanted_vals):
+	"""
+	Creates a dictionary containing the items from a_dict minus those whose
+	value is in unwanted_vals. a_dict is not modified.
+
+	Args:
+		a_dict (dict): any dictionary
+		unwanted_vals: a list, set or tuple containing the values to filter
+
+	Returns:
+		dict: a dictionary containing the items of a_dict except the unwanted
+		values
+	"""
 	wanted_items = dict()
 
 	for key, value in a_dict.items():
@@ -14,6 +37,18 @@ def filter_values_from_dict(a_dict, unwanted_vals):
 
 
 def parse_field_values(field_setting_path, allow_nones=False):
+	"""
+	Parses a YAML file that defines field values. The association of the
+	fields' name and value is stored in a dictionary returned by the function.
+
+	Args:
+		field_setting_path (pathlib.Path): the path to the YAML file
+		allow_nones (bool): if False, the returned dictionary does not contain
+			None values. Defaults to False.
+
+	Returns:
+		dict: a dictionary matching the fields' name with their value
+	"""
 	field_values = None
 	with field_setting_path.open(encoding="utf8") as field_setting_stream:
 		field_values = load(field_setting_stream, FullLoader)
@@ -25,6 +60,13 @@ def parse_field_values(field_setting_path, allow_nones=False):
 
 
 def print_dictionary(a_dict):
+	"""
+	Prints a dictionary's items in the console in the following format.
+	Key: value
+
+	Args:
+		a_dict (dict): any dictionary
+	"""
 	for key, value in a_dict.items():
 		print(str(key) + ": " + str(value))
 
