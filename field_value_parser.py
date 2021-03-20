609053e90dd922600e02fa7a2c19b4b3689f4911
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-20 00:35:37 -0400

    Filling format matched with fields

--- field_value_parser.py
+++ field_value_parser.py
@@ -24,6 +24,12 @@ from sys import argv
 from yaml import FullLoader, load
 
 
+_CHECKBOX_YES = "/Oui"
+_CHOICE1 = "/Choix1"
+_CHOICE2 = "/Choix2"
+_KEY_CHECKED = "Cochée"
+
+
 def _dict_key_val_str(key, value):
 	"""
 	Creates a string representing a dictionary item. The string fits this
@@ -54,6 +60,10 @@ def _dict_key_val_type_str(key, value):
 	return _dict_key_val_str(key, value) + " " + str(type(value))
 
 
+def _error_for_unexpected_value(key, value):
+	raise ValueError("Unexpected value for " + str(key) + ": " + str(value))
+
+
 def filter_values_from_dict(a_dict, unwanted_vals):
 	"""
 	Creates a dictionary containing the items from a_dict minus those whose
@@ -76,27 +86,137 @@ def filter_values_from_dict(a_dict, unwanted_vals):
 	return wanted_items
 
 
-def parse_field_values(field_setting_path, allow_nones=False):
+def get_yaml_content(field_setting_path, allow_nones=False):
 	"""
-	Parses a YAML file that defines field values. The association of the
-	fields' name and value is stored in a dictionary returned by the function.
+	Reads a YAML file and returns its content in in a dictionary.
 
 	Args:
 		field_setting_path (pathlib.Path): the path to the YAML file
-		allow_nones (bool): if False, the returned dictionary does not contain
+		allow_nones (bool): if False, the returned content does not contain
 			None values. Defaults to False.
 
 	Returns:
-		dict: a dictionary matching the fields' name with their value
+		dict: the YAML file's content
+
+	Raises:
+		ValueError: if the extension of field_setting_path is not "yaml" or
+		"yml"
 	"""
-	field_values = None
+	if field_setting_path.suffixes not in ([".yaml"], [".yml"]):
+		raise ValueError("The file extension must be \".yaml\" or \".yml\".")
+
+	yaml_content = None
 	with field_setting_path.open(encoding="utf8") as field_setting_stream:
-		field_values = load(field_setting_stream, FullLoader)
+		yaml_content = load(field_setting_stream, FullLoader)
 
 		if not allow_nones:
-			field_values = filter_values_from_dict(field_values, (None,))
+			yaml_content = filter_values_from_dict(yaml_content, (None,))
+
+	return yaml_content
+
+
+def parse_yaml_content(yaml_content):
+	field_values = dict()
+
+	for key, value in yaml_content.items():
+		if key == "RaisonVoyage":
+			field_values.update(_parse_travel_reasons(value))
+		elif key == "Dépenses":
+			field_values.update(_parse_expanse_list(value))
+		elif key == "Codes comptables":
+			field_values.update(_parse_codes_comptables(value))
+		elif key == "RaisonDépenses":
+			if value.lower() == "voyage":
+				field_values["Group1"] = _CHOICE1
+			elif value.lower() == "autre":
+				field_values["Group1"] = _CHOICE2
+			else:
+				_error_for_unexpected_value(key, value)
+		elif key == "Étudiant(e) ou employé(e)":
+			if value.lower() == "employé(e)":
+				field_values["Group2"] = _CHOICE1
+			elif value.lower() == "étudiant(e)":
+				field_values["Group2"] = _CHOICE2
+			else:
+				_error_for_unexpected_value(key, value)
+		elif key == "Chèque ou dépôt":
+			if value.lower() == "dépôt":
+				field_values["Group4"] = "UNDEFINED"
+			elif value.lower() == "chèque":
+				field_values["Group4"] = "/Ch#E8que"
+			else:
+				_error_for_unexpected_value(key, value)
+		elif key == "":
+			if value.lower() == "":
+				field_values["Group1"] = _CHOICE1
+			elif value.lower() == "":
+				field_values["Group1"] = _CHOICE2
+			else:
+				_error_for_unexpected_value(key, value)
+		elif key == "Distance":
+			field_values["KM"] = value
+		else:
+			field_values[key] = value
 
 	return field_values
+
+
+def _parse_codes_comptables(codes_comptables):
+	fields = dict()
+
+	for i in range(len(codes_comptables)):
+		cc = codes_comptables[i]
+		ubr = cc["UBR"]
+		account = cc["Compte"]
+		df = cc["DemFin"]
+		cbs = cc["CBS"]
+		amount = cc["Montant"]
+
+		fields["UBR" + str(i+1)] = ubr
+		fields["CC" + str(i+1)] = account
+		fields["DF" + str(i+1)] = df
+		fields["CBS" + str(i+1)] = cbs
+		fields["ccMontant$" + str(i+1)] = amount
+
+	return fields
+
+
+def _parse_expanse_list(expanse_list):
+	fields = dict()
+
+	for i in range(len(expanse_list)):
+		expanse = expanse_list[i]
+		description = expanse["Description"]
+		amount = expanse["Montant"]
+
+		fields["Détails" + str(i+1)] = description
+		fields["Montant$" + str(i+1)] = amount
+
+	return fields
+
+
+def _parse_travel_reasons(travel_reason_dict):
+	fields = dict()
+
+	presentation = travel_reason_dict["Présentation"]
+	if presentation[_KEY_CHECKED]:
+		fields["Boite1"] = _CHECKBOX_YES
+	fields["Présentation"] = presentation["Sujet"]
+
+	conference = travel_reason_dict["Conférence"]
+	if conference[_KEY_CHECKED]:
+		fields["Boite2"] = _CHECKBOX_YES
+	fields["Conférence"] = conference["Nom"]
+
+	if travel_reason_dict["Sabbatique"]:
+		fields["Boite3"] = _CHECKBOX_YES
+
+	others = travel_reason_dict["Autres"]
+	if others[_KEY_CHECKED]:
+		fields["Boite4"] = _CHECKBOX_YES
+	fields["Autres"] = others["Précision"]
+
+	return fields
 
 
 def print_dictionary(a_dict, print_val_type):
@@ -155,6 +275,6 @@ if __name__ == "__main__":
 		print(ve)
 		exit()
 
-
-	field_values = parse_field_values(field_setting_path)
+	yaml_content = get_yaml_content(field_setting_path)
+	field_values = parse_yaml_content(yaml_content)
 	print_dictionary(field_values, print_val_type)
