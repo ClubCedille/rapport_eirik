6b40fe6d5507c4b1adc6d8838361d20c7555b460
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-29 18:01:14 -0400

    if __name__ == '__main__' in write_page_objects.py

--- write_page_objects.py
+++ write_page_objects.py
@@ -30,45 +30,47 @@ def _make_default_output_file_stem(input_path):
 	return input_path.stem + "_object_hierarchy"
 
 
-# Input path checks
-try:
-	input_path = Path(argv[1])
-except IndexError:
-	print("ERROR! The path to a " + _INPUT_EXTENSION
-		+ " file must be provided as the first argument.")
-	exit()
+if __name__ == "__main__":
 
-if not input_path.exists():
-	print("ERROR! " + str(input_path) + " does not exist.")
-	exit()
+	# Input path checks
+	try:
+		input_path = Path(argv[1])
+	except IndexError:
+		print("ERROR! The path to a " + _INPUT_EXTENSION
+			+ " file must be provided as the first argument.")
+		exit()
 
-elif input_path.suffixes != _INPUT_EXTENSION_LIST: # False if not a file
-	print("ERROR! The first argument must be the path to a "
-		+ _INPUT_EXTENSION + " file.")
-	exit()
+	if not input_path.exists():
+		print("ERROR! " + str(input_path) + " does not exist.")
+		exit()
 
-# Output path checks
-try:
-	output_path = Path(argv[2])
+	elif input_path.suffixes != _INPUT_EXTENSION_LIST: # False if not a file
+		print("ERROR! The first argument must be the path to a "
+			+ _INPUT_EXTENSION + " file.")
+		exit()
 
-	if output_path.is_dir():
-		output_path = output_path/_make_default_output_file_name(input_path)
+	# Output path checks
+	try:
+		output_path = Path(argv[2])
 
-	elif output_path.suffixes != _OUTPUT_EXTENSION_LIST:
-		output_path = output_path.with_suffix(_OUTPUT_EXTENSION)
+		if output_path.is_dir():
+			output_path = output_path/_make_default_output_file_name(input_path)
 
-except IndexError:
-	output_path = input_path.with_name(
-		_make_default_output_file_name(input_path))
+		elif output_path.suffixes != _OUTPUT_EXTENSION_LIST:
+			output_path = output_path.with_suffix(_OUTPUT_EXTENSION)
 
-# Real work
-reader = PdfFileReader(input_path.open("rb"))
-pages = reader.pages
+	except IndexError:
+		output_path = input_path.with_name(
+			_make_default_output_file_name(input_path))
 
-with output_path.open("w") as output_stream:
-	output_stream.write("Object hierachy of " + str(input_path) + "\n")
+	# Real work
+	reader = PdfFileReader(input_path.open("rb"))
+	pages = reader.pages
 
-	for i in range(len(pages)):
-		page = pages[i]
-		output_stream.write("\n\nPAGE " + str(i) + "\n")
-		write_pdf_obj_struct(page, output_stream)
+	with output_path.open("w") as output_stream:
+		output_stream.write("Object hierachy of " + str(input_path) + "\n")
+
+		for i in range(len(pages)):
+			page = pages[i]
+			output_stream.write("\n\nPAGE " + str(i) + "\n")
+			write_pdf_obj_struct(page, output_stream)
