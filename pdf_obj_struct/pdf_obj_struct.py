32bf0f8a664908aa677d2549a105fc13e40dd266
GRV96 <32419088+GRV96@users.noreply.github.com>
2021-04-01 16:56:36 -0400

    Maximum recursion depth specified for scripts in pdf_obj_struct (#15)
    * A script argument specifies the maximum recursion depth.
    * The value of BooleanOjbects is written correctly.
    * PDF object structures can be written in the console.

--- pdf_obj_struct/pdf_obj_struct.py
+++ pdf_obj_struct/pdf_obj_struct.py
@@ -5,19 +5,25 @@ embedded in one another and other objects.
 """
 
 
-from PyPDF2.generic import IndirectObject
+from PyPDF2.generic import BooleanObject, IndirectObject
 
 
 _DLST = (dict, list, set, tuple)
-
-
 _LT = (list, tuple)
-
 
 _STREAM_WRITING_MODES = ("a", "a+", "r+", "w", "w+")
 
-
+_CLOSING_BRACET_COLON_SPACE = "]: "
+_COLON_SPACE = ": "
+_NEW_LINE = "\n"
+_OPENING_BRACKET = "["
+_SPACE = " "
 _TAB = "\t"
+_UNEXPLORED_OBJS = "\t[...]\n"
+
+
+def _index_between_brackets(index):
+	return _OPENING_BRACKET + str(index) + _CLOSING_BRACET_COLON_SPACE
 
 
 def _make_tabs(n):
@@ -25,7 +31,11 @@ def _make_tabs(n):
 
 
 def _obj_and_type_to_str(obj):
-	return str(obj) + " " + str(type(obj))
+	if isinstance(obj, BooleanObject):
+		return str(obj.value) + _SPACE + str(type(obj))
+
+	else:
+		return str(obj) + _SPACE + str(type(obj))
 
 
 def obj_is_a_dlst(obj):
@@ -43,7 +53,7 @@ def obj_is_a_dlst(obj):
 	return isinstance(obj, _DLST)
 
 
-def _res_pdf_ind_object(obj):
+def _rslv_pdf_ind_object(obj):
 	if isinstance(obj, IndirectObject):
 		return obj.getObject()
 
@@ -55,8 +65,8 @@ def _return_arg(obj):
 	return obj
 
 
-def write_pdf_obj_struct(struct, w_stream,
-		write_types=False, res_ind_objs=False):
+def write_pdf_obj_struct(struct, w_stream, write_types=False,
+		rslv_ind_objs=False, depth_limit=0):
 	"""
 	Writes a PDF object structure in a file stream. The indentation indicates
 	which objects are contained in others. The stream's mode must be "a",
@@ -70,10 +80,12 @@ def write_pdf_obj_struct(struct, w_stream,
 			structure's representation
 		write_types (bool): If True, this method will write the contained
 			objects' type in the stream. Defaults to False.
-		res_ind_objs (bool): If True, the indirect objects found in the
+		rslv_ind_objs (bool): If True, the indirect objects found in the
 			structure will be resolved. Defaults to False. WARNING! Setting
 			this parameter to True can make the function exceed the maximum
 			recursion depth.
+		depth_limit (int): a limit to the recursion depth. If it is set to 0
+			or less, no limit is enforced. Defaults to 0.
 
 	Raises:
 		RecursionError: if this function exceeds the maximum recursion depth
@@ -84,55 +96,65 @@ def write_pdf_obj_struct(struct, w_stream,
 			+ "\"a\", \"a+\", \"r+\", \"w\" or \"w+\".")
 
 	obj_str_fnc = _obj_and_type_to_str if write_types else str
-	ind_obj_fnc = _res_pdf_ind_object if res_ind_objs else _return_arg
+	ind_obj_fnc = _rslv_pdf_ind_object if rslv_ind_objs else _return_arg
 
 	if obj_is_a_dlst(struct):
-		w_stream.write(str(type(struct)) + "\n")
-		indent = 1
+		w_stream.write(str(type(struct)) + _NEW_LINE)
+		rec_depth = 1
 
 	else:
-		indent = 0
+		rec_depth = 0
 
-	_write_pdf_obj_struct_rec(struct, w_stream, indent,
-		obj_str_fnc, ind_obj_fnc)
+	_write_pdf_obj_struct_rec(struct, w_stream, rec_depth,
+		depth_limit, obj_str_fnc, ind_obj_fnc)
 
 
-def _write_pdf_obj_struct_rec(obj_to_write, w_stream, indent,
-		obj_str_fnc, ind_obj_fnc):
-	tabs = _make_tabs(indent)
-	indent += 1
+def _write_pdf_obj_struct_rec(obj_to_write, w_stream, rec_depth,
+		depth_limit, obj_str_fnc, ind_obj_fnc):
+	tabs = _make_tabs(rec_depth)
+	rec_depth += 1
 
 	if isinstance(obj_to_write, _LT):
 		length = len(obj_to_write)
 
 		for i in range(length):
 			item = ind_obj_fnc(obj_to_write[i])
-			line = tabs + "[" + str(i) + "]: "
+			line = tabs + _index_between_brackets(i)
 
 			if obj_is_a_dlst(item):
 				line += str(type(item))
-				w_stream.write(line + "\n")
-				_write_pdf_obj_struct_rec(item, w_stream, indent,
-					obj_str_fnc, ind_obj_fnc)
+				w_stream.write(line + _NEW_LINE)
+
+				if depth_limit<=0 or rec_depth<=depth_limit:
+					_write_pdf_obj_struct_rec(item, w_stream, rec_depth,
+						depth_limit, obj_str_fnc, ind_obj_fnc)
+
+				else:
+					w_stream.write(tabs + _UNEXPLORED_OBJS)
 
 			else:
 				line += obj_str_fnc(item)
-				w_stream.write(line + "\n")
+				w_stream.write(line + _NEW_LINE)
 
 	elif isinstance(obj_to_write, dict):
 		for key, value in obj_to_write.items():
 			value = ind_obj_fnc(value)
-			line = tabs + str(key) + ": "
+			line = tabs + str(key) + _COLON_SPACE
 
 			if obj_is_a_dlst(value):
 				line += str(type(value))
-				w_stream.write(line + "\n")
-				_write_pdf_obj_struct_rec(value, w_stream, indent,
-					obj_str_fnc, ind_obj_fnc)
+				w_stream.write(line + _NEW_LINE)
+
+				if depth_limit<=0 or rec_depth<=depth_limit:
+					_write_pdf_obj_struct_rec(value, w_stream, rec_depth,
+						depth_limit, obj_str_fnc, ind_obj_fnc)
+
+				else:
+					w_stream.write(tabs + _UNEXPLORED_OBJS)
 
 			else:
 				line += obj_str_fnc(value)
-				w_stream.write(line + "\n")
+				w_stream.write(line + _NEW_LINE)
 
 	elif isinstance(obj_to_write, set):
 		for item in obj_to_write:
@@ -141,14 +163,19 @@ def _write_pdf_obj_struct_rec(obj_to_write, w_stream, indent,
 
 			if obj_is_a_dlst(item):
 				line += str(type(item))
-				w_stream.write(line + "\n")
-				_write_pdf_obj_struct_rec(item, w_stream, indent,
-					obj_str_fnc, ind_obj_fnc)
+				w_stream.write(line + _NEW_LINE)
+
+				if depth_limit<=0 or rec_depth<=depth_limit:
+					_write_pdf_obj_struct_rec(item, w_stream, rec_depth,
+						depth_limit, obj_str_fnc, ind_obj_fnc)
+
+				else:
+					w_stream.write(tabs + _UNEXPLORED_OBJS)
 
 			else:
 				line += obj_str_fnc(item)
-				w_stream.write(line + "\n")
+				w_stream.write(line + _NEW_LINE)
 
 	else:
 		line = tabs + obj_str_fnc(obj_to_write)
-		w_stream.write(line + "\n")
+		w_stream.write(line + _NEW_LINE)
