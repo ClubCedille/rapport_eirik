7d12595883aaf51e02fbb52b1d31eceff1bf6242
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-04-22 14:05:33 -0400

    Unused import

--- fill_expense_report.py
+++ fill_expense_report.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from PyPDF2 import PdfFileReader
 from pypdf2_util import make_writer_from_reader, RadioBtnGroup,\
 	set_need_appearances, update_page_fields
-from sys import argv, exit
+from sys import argv
 
 
 if __name__ == "__main__":
