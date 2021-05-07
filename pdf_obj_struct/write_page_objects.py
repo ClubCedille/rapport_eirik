839c882b3a10400565c05dcce298e22f7939125d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-05-07 14:31:48 -0400

    Argparse used in pdf_obj_struct scripts

--- pdf_obj_struct/write_page_objects.py
+++ pdf_obj_struct/write_page_objects.py
@@ -1,21 +1,28 @@
-"""
-Explores recursively the object structure in a PDF file's pages and records
-their structure in a .txt file.
-
-Args:
-	1: (str) the path to the PDF file to explore
-	2: (int, optional) the limit to the recursion depth that the algorithm
-		can reach. If 0 or less, no limit is set, and PyPDF2's indirect
-		objects will not be resolved. Defaults to 0.
-	3: (str, optional) the path to the .txt output file. If set to "console",
-		the object structure will be written in the console.
-"""
-
-
+from argparse import ArgumentParser
 from pathlib import Path
 from pdf_obj_struct import write_pdf_obj_struct
 from PyPDF2 import PdfFileReader
 from sys import argv
+
+
+parser = ArgumentParser(description=
+	"This script explores recursively the object structure in a PDF file's\
+	pages and records it in a .txt file.")
+
+parser.add_argument("-f", "--file", type=Path, required=True,
+	help="Path to the PDF file to explore")
+
+parser.add_argument("-d", "--depth", type=int, default=0,
+	help="Limit of the recursion depth that the algorithm can reach. If 0 or\
+	less, no limit is set, and PyPDF2's indirect objects will not be\
+	resolved. Defaults to 0.")
+
+parser.add_argument("-o", "--output", type=Path,
+	help="Path to the .txt output file. If set to \"console\", the object\
+	structure will be written in the console. If not specified, an output\
+	path is generated automatically.")
+
+args = parser.parse_args()
 
 
 _INPUT_EXTENSION = ".pdf"
@@ -32,7 +39,7 @@ def _make_default_output_file_stem(input_path):
 	return input_path.stem + "_page_objects"
 
 
-def _write_page_objs_in_console(pdf_path, pages, w_stream, depth_limit):
+def _write_page_objs_in_stream(pdf_path, pages, w_stream, depth_limit):
 	w_stream.write("Objects in the pages of " + str(pdf_path) + "\n")
 
 	for i in range(len(pages)):
@@ -44,12 +51,7 @@ def _write_page_objs_in_console(pdf_path, pages, w_stream, depth_limit):
 if __name__ == "__main__":
 
 	# Input path checks
-	try:
-		input_path = Path(argv[1])
-	except IndexError:
-		print("ERROR! The path to a " + _INPUT_EXTENSION
-			+ " file must be provided as the first argument.")
-		exit()
+	input_path = args.file
 
 	if not input_path.exists():
 		print("ERROR! " + str(input_path) + " does not exist.")
@@ -61,37 +63,24 @@ if __name__ == "__main__":
 			+ _INPUT_EXTENSION + " file.")
 		exit()
 
-	# Recursion depth limit check
-	try:
-		depth_limit = int(argv[2])
-
-	except IndexError:
-		depth_limit = 0
-
-	except ValueError as ve:
-		print("Argument 2: " + str(ve))
-		exit()
+	# Recursion depth limit
+	depth_limit = args.depth
 
 	# Output path checks
-	try:
-		output_path = argv[3]
+	output_path = args.output
 
-		if output_path.lower() == "console":
-			output_path = None
-
-		else:
-			output_path = Path(output_path)
-
-			if output_path.is_dir():
-				output_path =\
-					output_path/_make_default_output_file_name(input_path)
-
-			elif output_path.suffixes != _OUTPUT_EXTENSION_IN_LIST:
-				output_path = output_path.with_suffix(_OUTPUT_EXTENSION)
-
-	except IndexError:
+	if output_path is None:
 		output_path = input_path.with_name(
 			_make_default_output_file_name(input_path))
+
+	elif str(output_path).lower() == "console":
+			output_path = None
+
+	elif output_path.is_dir():
+		output_path = output_path/_make_default_output_file_name(input_path)
+
+	elif output_path.suffixes != _OUTPUT_EXTENSION_IN_LIST:
+		output_path = output_path.with_suffix(_OUTPUT_EXTENSION)
 
 	# Real work
 	reader = PdfFileReader(input_path.open(mode="rb"), strict=False)
@@ -99,9 +88,9 @@ if __name__ == "__main__":
 
 	if output_path is None:
 		from sys import stdout
-		_write_page_objs_in_console(input_path, pages, stdout, depth_limit)
+		_write_page_objs_in_stream(input_path, pages, stdout, depth_limit)
 
 	else:
 		with output_path.open(mode="w", encoding="utf8") as output_stream:
-			_write_page_objs_in_console(input_path, pages,
+			_write_page_objs_in_stream(input_path, pages,
 				output_stream, depth_limit)
