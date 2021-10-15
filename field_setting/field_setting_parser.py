0db3bc31832d25dd17c856cb16bfbb364d5e0c65
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-10-15 12:32:07 -0400

    Jazal 1.0.0 in field_setting_parser

--- field_setting/field_setting_parser.py
+++ field_setting/field_setting_parser.py
@@ -291,6 +291,7 @@ def str_to_bool(bool_str):
 
 if __name__ == "__main__":
 	from argparse import ArgumentParser
+	from path_checks import check_mandatory_path
 
 	parser = ArgumentParser(description=__doc__)
 
@@ -306,6 +307,8 @@ if __name__ == "__main__":
 
 	field_setting_path = args.file
 	print_val_type = args.types
+
+	check_mandatory_path(field_setting_path, "-f/--file", (".yml",))
 
 	yaml_content = get_yaml_content(field_setting_path)
 	field_values = parse_yaml_content(yaml_content)
